15acabbacdc4ad618cc7d2191df40113446154fe
mazen hosam <mazenjr2005@gmail.com>
2024-10-30 10:04:22 +0300

    'new'

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,21 +2,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-// App.jsx
+// src/App.jsx
 import React from 'react';
-import Header from './Header';
-import MainContent from './MainContent';
-import Footer from './Footer';
+import UserProfile from './components/UserProfile';
 
 function App() {
     return (
         <div>
-            <Header />
-            <MainContent />
-            <Footer />
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
         </div>
     );
 }
 
 export default App;
-
