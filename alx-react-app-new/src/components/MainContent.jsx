65a38a456621e4a537403890a23baf4cdc7087d6
mazen hosam <mazenjr2005@gmail.com>
2024-11-05 09:51:57 +0200

    new

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function MainContent() {
   return (
     <main style={{ padding: '20px', backgroundColor: '#f4f4f4', color: '#333' }}>
-      <p>This is the main content area.</p>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
     </main>
   );
 }
