51be235f4985ee2163853f4ea690e98514fb12f1
mazen hosam <mazenjr2005@gmail.com>
2024-11-12 01:22:34 +0200

    new app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
-import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './RecipeList';
+import AddRecipeForm from './AddRecipeForm';
 import useRecipeStore from './recipeStore';
 
 const App = () => {
