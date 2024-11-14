4e5379e948478d0141018a81677c9856ece243b1
mazen hosam <mazenjr2005@gmail.com>
2024-11-14 16:52:45 +0200

    fix

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,7 @@
 // src/components/RecipeList.js
 import { useRecipeStore } from '../recipeStore';
+import { Link } from 'react-router-dom';
+
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
 
@@ -7,7 +9,9 @@ const RecipeList = () => {
     <div>
       {recipes.map((recipe) => (
         <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
+          <h3>
+            <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+          </h3>
           <p>{recipe.description}</p>
         </div>
       ))}
