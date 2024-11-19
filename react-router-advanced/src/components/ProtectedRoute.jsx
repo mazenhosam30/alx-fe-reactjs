7f14159a86a5d23fa3c08293460800e181fae32b
mazen hosam <mazenjr2005@gmail.com>
2024-11-19 20:24:17 +0200

    advanced

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
+"useAuth"
+
 const ProtectedRoute = ({ isAuthenticated, children }) => {
     return isAuthenticated ? children : <Navigate to="/" />;
 };
