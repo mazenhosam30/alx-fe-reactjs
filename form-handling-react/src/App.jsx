98f81efec5c67354b7c98e22c5c06ff182ab7fbb
mazen hosam <mazenjr2005@gmail.com>
2024-11-19 18:45:52 +0200

    new

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,5 +1,13 @@
-import FormikForm from "./components/FormikForm"
-import RegistrationForm from "./components/RegistrationForm"
+import React from "react";
+import FormikForm from "./components/FormikForm";
 
+function App() {
+    return (
+        <div>
+            <h1>Registration Form</h1>
+            <FormikForm />
+        </div>
+    );
+}
 
-export default App
+export default App;
