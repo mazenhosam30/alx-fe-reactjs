850a2b0373fe9cdb74140c1bb3019c60a444a267
mazen hosam <mazenjr2005@gmail.com>
2024-11-27 20:09:03 +0200

    tailwind

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 mx-auto"
+        className="rounded-full w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 mx-auto md:max-w-sm"
       />
       <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4 text-center sm:p-4">
         John Doe
