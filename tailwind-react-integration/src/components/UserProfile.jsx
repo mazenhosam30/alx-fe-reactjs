24a1d1cc4465c26e65ada96464174f5dc5f21e27
mazen hosam <mazenjr2005@gmail.com>
2024-11-27 18:49:57 +0200

    tailwind

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm md:max-w-md mx-auto my-10 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-4 sm:p-6 md:p-8 max-w-xs sm:max-w-sm md:max-w-md mx-auto my-10 rounded-lg shadow-lg sm:p-4 ">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
         className="rounded-full w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 mx-auto"
       />
-      <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4 text-center">
+      <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4 text-center sm:p-4">
         John Doe
       </h1>
       <p className="text-sm sm:text-base md:text-lg text-gray-600 text-center">
