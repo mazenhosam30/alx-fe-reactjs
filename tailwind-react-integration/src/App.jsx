d2e96d813eebfef71ba34f76c7bbf3a4f88a15b2
mazen hosam <mazenjr2005@gmail.com>
2024-11-27 17:54:47 +0200

    tailwind

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-100">
+      <h1 className="text-3xl font-bold text-blue-600">Tailwind CSS is working!</h1>
+    </div>
+  );
+}
+
+export default App;
+
