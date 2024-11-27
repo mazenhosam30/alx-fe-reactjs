32514e77a5e82e1c0fc9fa1a0e149fcc64dbf1da
mazen hosam <mazenjr2005@gmail.com>
2024-11-27 18:41:51 +0200

    tailwind

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,10 +1,11 @@
+import UserProfile from "./components/UserProfile";
+
 function App() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-blue-100">
-      <h1 className="text-3xl font-bold text-blue-600">Tailwind CSS is working!</h1>
+    <div>
+      <UserProfile />
     </div>
   );
 }
 
 export default App;
-
