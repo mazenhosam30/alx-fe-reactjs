59f9d2b0bfd1a55e1c1dabef73542d483db29a4c
mazen hosam <mazenjr2005@gmail.com>
2024-12-04 21:25:30 +0200

    Removed filename.txt

--- recipe-sharing-platfom/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // Set to 'media' or 'class' if needed
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
